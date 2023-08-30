42649855aed226f3c530333ced9b06ca8e37fab8
Luis Escobar <luisaeniebla@gmail.com>
2023-08-29 19:36:08 -0700

    learned about grids

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,13 +4,14 @@ interface CardProps {
     title: string,
     picture: string,
     color: string,
+    layout: string,
 }
 
-const Card: FC<CardProps> = ({ title, picture, color }) => {
+const Card: FC<CardProps> = ({ title, picture, color, layout }) => {
     return (
-        <div className={`${color}` + ' relative rounded-2 w-full h-full'}>
+        <div className={`${layout}` + `${color}` + ' relative rounded-[8px]'}>
             <img src={picture} alt="picture" className=' absolute top-6 right-6' />
-            <h1 className='text-white absolute bottom-6 left-6'>{title}</h1>
+            <h1 className='text-white font-bold absolute bottom-6 left-6'>{title}</h1>
         </div>
     )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,24 +5,22 @@ import Card from './Card'
 
 const Hero = ({ }) => {
   return (
-    <div className=" container px-4 pt-4 mb-[100px]">
+    <div className=" container px-4 pt-4 mb-[100px] flex flex-col gap-8">
       <div className="flex justify-between">
         <img src="./logo.svg" alt="logo" className='' />
         <div className='flex items-center p- bg-black text-biege font-bold rounded-[38px] py-2 px-7'> Free Consultation</div>
       </div>
-      <div className='flex flex-col gap-4'>
-        <h1 className="text-h-lg text-black font-bold">Design solutions made easy</h1>
+      <div className='flex flex-col gap-4 text-center'>
+        <h1 className="text-h-md text-black font-bold">Design solutions made easy</h1>
         <p className='text-b-md text-grey'>With over ten years of experience in various design disciplines, Im your one-stop shop for your design needs.</p>
       </div>
-      <div className='flex flex-col gap-6'>
-        <Card title='Graphic Design' picture='./pattern-graphic-design.svg' color='bg-purple' />
-        <div className='flex'>
-          <Card title='UI/UX' picture='./pattern-ui-ux.svg' color='bg-orange' />
-          <Card title='Apps' picture='./pattern-apps.svg' color='bg-pink' />
-        </div>
-        <Card title='Illustrations' picture='./pattern-illustrations.svg' color='bg-salmon' />
-        <Card title='Photography' picture='./pattern-photography.svg' color='bg-teal' />
-        <Card title='Motion Graphics' picture='./pattern-motion-graphics.svg' color='bg-magenta' />
+      <div className='grid grid-cols-2 gap-6 w-full h-[1164px]'>
+        <Card title='Graphic Design' picture='./pattern-graphic-design.svg' color='bg-purple' layout='col-span-2 row-span-2 ' />
+        <Card title='UI/UX' picture='./pattern-ui-ux.svg' color='bg-orange' layout='' />
+        <Card title='Apps' picture='./pattern-apps.svg' color='bg-pink' layout='' />
+        <Card title='Illustrations' picture='./pattern-illustrations.svg' color='bg-salmon' layout='col-span-2 ' />
+        <Card title='Photography' picture='./pattern-photography.svg' color='bg-teal' layout='col-span-2 ' />
+        <Card title='Motion Graphics' picture='./pattern-motion-graphics.svg' color='bg-magenta' layout='col-span-2 ' />
       </div>
     </div>
   )
