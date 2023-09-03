23a7c07b50552d816a994cf5cb7267d32442a5c1
Luis Escobar <luisaeniebla@gmail.com>
2023-09-03 15:08:00 -0700

    We tried homebrewing carousel

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,11 +1,43 @@
-import { FC } from 'react'
+'use client'
+import { FC, useEffect, useRef } from 'react'
 
 
 const Works = ({ }) => {
+
+  const leftArrowRef = useRef<HTMLDivElement>(null)
+  const rightArrowRef = useRef<HTMLDivElement>(null)
+  const carouselRef = useRef<HTMLDivElement>(null)
+
+  const rotateLeft = () => {
+    const carousel = carouselRef.current
+    if (!carousel) return
+
+
+
+    return
+  }
+  const rotateRight = () => {
+    return
+  }
+
+  useEffect(() => {
+    const leftArrow = leftArrowRef.current
+    const rightArrow = rightArrowRef.current
+    if (!leftArrow || !rightArrow) return
+
+    leftArrow.addEventListener("click", rotateLeft)
+    rightArrow.addEventListener("click", rotateRight)
+
+    return () => {
+      leftArrow.removeEventListener("click", rotateLeft)
+      rightArrow.removeEventListener("click", rotateRight)
+    }
+  }, [])
+
   return (
     <div className="flex flex-col text-center">
       <h2 className="text-h-md font-bold">My Work</h2>
-      <div className="carousel max-w-full flex whitespace-nowrap overflow-hidden gap-[30px]">
+      <div ref={carouselRef} className="carousel max-w-full flex whitespace-nowrap overflow-hidden gap-[30px]">
         <img src="./image-slide-1.jpg" alt="work" className='w-[270px] object-cover rounded-xl' />
         <img src="./image-slide-2.jpg" alt="work" className='w-[270px] object-cover rounded-xl' />
         <img src="./image-slide-3.jpg" alt="work" className='w-[270px] object-cover rounded-xl' />
@@ -13,10 +45,10 @@ const Works = ({ }) => {
         <img src="./image-slide-5.jpg" alt="work" className='w-[270px] object-cover rounded-xl' />
       </div>
       <div className="flex mx-auto gap-4">
-        <div className="bg-black rounded-full w-[64px] h-[64px] p-6">
+        <div ref={leftArrowRef} className="bg-black rounded-full w-[64px] h-[64px] p-6">
           <img src="./icon-arrow-left.svg" alt="arrow" />
         </div>
-        <div className="bg-black rounded-full w-[64px] h-[64px] p-6">
+        <div ref={rightArrowRef} className="bg-black rounded-full w-[64px] h-[64px] p-6">
           <img src="./icon-arrow-right.svg" alt="arrow" />
         </div>
       </div>
